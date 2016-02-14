5d50b14f4c0e3b186379d7ae18912adedd152644
Kunal Desai <kunaljaydesai@gmail.com>
2016-02-13 22:29:39 -0800

    lol

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/KunalDesai/Documents/SideProjects/learn_django/twitter_crm/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
